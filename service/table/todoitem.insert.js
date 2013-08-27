f2f692fbfb892270f4768d2e1ad95b2cfcad32a8
egamma@microsoft.com <egamma@microsoft.com>
2013-08-27 14:09:18 +0000

    better

--- service/table/todoitem.insert.js
+++ service/table/todoitem.insert.js
@@ -1,6 +1,6 @@
 function insert(item, user, request) {
     //console.log(item);
-    item.text =  item.text + "checked by Monaco enriched insert script";
+    item.text =  item.text + " -- approved";
     request.execute();
 
 }
