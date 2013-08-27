3ba4e4985f33e1f11fec68542bc9c9ffc85c3dde
egamma@microsoft.com <egamma@microsoft.com>
2013-08-27 14:05:33 +0000

    rich

--- service/table/todoitem.insert.js
+++ service/table/todoitem.insert.js
@@ -1,6 +1,6 @@
 function insert(item, user, request) {
     //console.log(item);
-    item.text = "<" + item.text + ">";
+    item.text =  item.text + "checked by Monaco enriched insert script";
     request.execute();
 
 }
