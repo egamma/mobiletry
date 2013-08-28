cf1525617ba7a9b9dca31504302f722b3cf52b7d
egamma@microsoft.com <egamma@microsoft.com>
2013-08-28 14:20:25 +0000

    using shared module from an API

--- service/api/todo.js
+++ service/api/todo.js
@@ -1,3 +1,6 @@
+
+var random = require('../shared/node_modules/random-to');  // need to add an absolute reference 'random-to' doesn't work', modules need to be in the shared folder
+
 exports.post = function(request, response) {
     // Use "request.service" to access features of your mobile service, e.g.:
     //   var tables = request.service.tables;
@@ -10,6 +13,5 @@ exports.get = function(request, response) {
     // Use "request.service" to access features of your mobile service, e.g.:
     //   var tables = request.service.tables;
     //   var push = request.service.push;
-
-    response.send(200, "Hello World");
+    response.send(200, "Hello World " + random.from0upto(100));
 };
