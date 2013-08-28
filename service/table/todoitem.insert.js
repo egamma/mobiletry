8e011b44d90a72b2d992712c57006b4536d82477
egamma@microsoft.com <egamma@microsoft.com>
2013-08-28 14:05:06 +0000

    polish

--- service/table/todoitem.insert.js
+++ service/table/todoitem.insert.js
@@ -1,9 +1,8 @@
 var validator = require('../shared/validator.js');
-var random = require('../shared/node_modules/random-to');
+var random = require('../shared/node_modules/random-to');  // need to add an absolute reference 'random-to' doesn't work', modules need to be in the shared folder
 	
 function insert(item, user, request) {
     //console.log(item);
-//    item.text =  item.text + " -- approved";
-    item.text =  item.text + validator.validate() + random.from0upto(10);
+    item.text =  item.text + validator.validate() + "by " + random.from0upto(100);
     request.execute();
 }
