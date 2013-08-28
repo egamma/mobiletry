780d95e58011e45a4bdf020d10d583803f9ddcf8
egamma@microsoft.com <egamma@microsoft.com>
2013-08-28 13:44:25 +0000

    adding a module

--- service/table/todoitem.insert.js
+++ service/table/todoitem.insert.js
@@ -1,8 +1,9 @@
 var validator = require('../shared/validator.js');
+var random = require('random-to');
 	
 function insert(item, user, request) {
     //console.log(item);
 //    item.text =  item.text + " -- approved";
-    item.text =  item.text + validator.validate();
+    item.text =  item.text + validator.validate() + random.from0upto(10);
     request.execute();
 }
