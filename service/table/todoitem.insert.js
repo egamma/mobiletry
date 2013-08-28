a461a04beb0828194ab2a4d72b7d07ee81f1c3ac
egamma@microsoft.com <egamma@microsoft.com>
2013-08-28 13:23:39 +0000

    adding shared code

--- service/table/todoitem.insert.js
+++ service/table/todoitem.insert.js
@@ -1,5 +1,8 @@
+var validator = require('../shared/validator.js');
+	
 function insert(item, user, request) {
     //console.log(item);
-    item.text =  item.text + " -- approved";
+//    item.text =  item.text + " -- approved";
+    item.text =  item.text + validator.validate();
     request.execute();
 }
