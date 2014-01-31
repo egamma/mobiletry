740054383198ca66de71504a1ffb10dc63d64b08
windowsazure <windowsazure>
2014-01-31 20:00:12 +0000

    first

--- service/scheduler/job1.js
+++ service/scheduler/job1.js
@@ -1,3 +1,3 @@
 function job1() {
-    console.warn("[portal] You are running an empty scheduled job. Update the script for job 'job1' or disable the job.");
+    console.warn("[monaco1] You are running an empty scheduled job. Update the script for job 'job1' or disable the job.");
 }
