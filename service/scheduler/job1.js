5c27773ae177f911fc1452390a9828b7b3ef165c
windowsazure <windowsazure>
2014-01-31 19:56:59 +0000

    Committing update from request /scmvfs/service/scheduler/job1.js

--- service/scheduler/job1.js
+++ service/scheduler/job1.js
@@ -1,3 +1,3 @@
 function job1() {
-    console.warn("You are running an empty scheduled job. Update the script for job 'job1' or disable the job.");
+    console.warn("[portal] You are running an empty scheduled job. Update the script for job 'job1' or disable the job.");
 }
