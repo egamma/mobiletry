d0de63cb7f26d175ec9a37dd861e83a4deaac324
windowsazure <windowsazure>
2014-02-06 16:14:39 +0000

    some change

--- service/scheduler/job1.js
+++ service/scheduler/job1.js
@@ -1,3 +1,3 @@
 function job1() {
-    console.warn("[monaco999] You are running an empty scheduled job. Update the script for job 'job1' or disable the job.");
+    console.warn("[monaco1000] You are running an empty scheduled job. Update the script for job 'job1' or disable the job.");
 }
--- service/table/todoitem.insert.js
+++ service/table/todoitem.insert.js
@@ -1,7 +1,7 @@
 /*global tables*/
 var random = require('../shared/node_modules/random-to');  // need to add an absolute reference 'random-to' doesn't work', modules need to be in the shared folder
 var logger = require('../shared/logger.js');
-	
+
 function insert(item, user, request) {
     //console.log(item);
 	
