75af163f9bd0dbe55a88f83f332fa574f5b80b7f
windowsazure <windowsazure>
2014-03-05 13:13:39 +0000

    Committing update from request /scmvfs/service/table/TodoItem.insert.js

--- service/table/todoitem.insert.js
+++ service/table/todoitem.insert.js
@@ -1,7 +1,7 @@
 
 var random = require('../shared/node_modules/random-to');  // need to add an absolute reference 'random-to' doesn't work', modules need to be in the shared folder
 var logger = require('../shared/logger');
-
+// changed vs
 function insert(item, user, request) {
     console.log(item);
     //tables.getTables('log');
